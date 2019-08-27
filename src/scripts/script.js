f09b293c03c9661a6f6827f28268fd9385affc75
lmsamples <lmarcuss2000@gmail.com>
2019-08-27 14:24:06 -0400

    caught up on daily journal

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,18 @@
 entryArray = [
     {
-        title: "I'm exhausted",
+        concept: "Defining Variables",
         date: "8/12/2019",
-        content: "javascript hurts my head"
+        content: "javascript hurts my head",
+        mood: "bored"
     },
     {
-        title: "I wanna sleep",
+        concept: "Looping Arrays",
         date: "8/113/2019",
-        content: "I'm gonna eat at schlotsky's"
+        content: "I'm gonna eat at schlotsky's",
+        mood: "tired"
     }
 ]
+
 console.log(entryArray)
 
 
@@ -19,26 +22,29 @@ var submit = document.querySelector("#entry-submit")
 
 submit.addEventListener("click", function(){
 
-    let entryTitle = document.querySelector("#entry-title").value
+    let entryConcept = document.querySelector("#entry-concept").value
     let entryDate = document.querySelector("#entry-date").value
     let entryContent = document.querySelector("#entry-content").value
+    let entryMood = document.querySelector("#entry-mood").value
 
     var entryString = {
-    title: entryTitle,
+    concept: entryConcept,
     date: entryDate,
-    content: entryContent
+    content: entryContent,
+    mood: entryMood
     }
     entryArray.push(entryString)
     console.log(entryArray)
 
     for (i = 0 ; i < entryArray.length ; i++){
 
-        const domString = `
-            <h2>${entryArray[i].title}</h2>
+        content.innerHTML += `
+        <article>
+            <h2>${entryArray[i].concept}</h2>
             <h4>${entryArray[i].date}</h4>
             <p>${entryArray[i].content}</p>
+            <p>${entryArray[i].mood}</p>
+        </article>
         `
-        console.log(domString)
-        content.innerHTML += domString
     }
 })
